204dae2558956219d89e7cb8fd94094fc175de75
chen_h <chen_h@outlook.com>
2022-05-25 18:44:39 -0500

    Update BRStudio.R

--- BRStudio.R
+++ BRStudio.R
@@ -1,6 +1,6 @@
 # BRStudio v2.1.3
 # This script exploits multiple threads for parallel computing
-# This script has been tested under Pop!_OS 20.04. If you are using it under other systems, please make sure that all packages are properly installed
+# This script has been tested under Kubuntu 22.04 LTS. If you are using it under other systems, please make sure that all packages are properly installed
 # You may run it with multiple panel files, but all of which should be formatted to 4 columns: Gene, Transcript_ID, Protein_ID, Protein_ID_for_PP2
 # You may run it with multiple pool files, but all of which should be formatted to 2 columns: Sample, Diagnosis
 # This script will only process ".genome.vcf" files. If other vcf files are found, they will not be processed but only be archived
