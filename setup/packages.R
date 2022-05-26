7f6f3b8762744749e6a5827d4ba459d81eacdbe0
chen_h <chen_h@outlook.com>
2022-05-26 00:03:29 -0500

    Delete setup directory

--- setup/packages.R
+++
@@ -1,1 +0,0 @@
-install.packages(c("devtools", "BiocManager", "tidyverse", "readxl", "writexl", "expss", "vcfR", "filesstrings", "R.utils", "car", "foreach", "doParallel", "rJava", "RSelenium"))
--- setup/webdriver.R
+++
@@ -1,1 +0,0 @@
-wdman::chrome(version = "latest")
