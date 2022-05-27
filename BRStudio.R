c4003e0bf66f41a0fa9d925dac50be1bee57e6d2
chen_h <chen_h@outlook.com>
2022-05-27 10:22:43 -0500

    Update BRStudio.R

--- BRStudio.R
+++ BRStudio.R
@@ -1,13 +1,14 @@
 # BRStudio v3.0.2
 # This script exploits multiple threads for parallel computing
 # This script has been tested under Kubuntu 22.04 LTS. If you are using it under other systems, please make sure that all packages are properly installed
-# You may run it with multiple panel files, but all of which should be formatted to 4 columns: Gene, Transcript_ID, Protein_ID, Protein_ID_for_PP2
+# You may run it with multiple panel files, but all of which should be formatted to 5 columns: Gene, Transcript_ID, Protein_ID, Protein_ID_for_PP2, ENSG_ID
 # You may run it with multiple pool files, but all of which should be formatted to 2 columns: Sample, Diagnosis
 # This script will only process ".genome.vcf" files. If other vcf files are found, they will not be processed but only be archived
-# Running no more than 96 samples in each batch is recommended
-# For each batch, 20 to 30 minutes might be required, please make sure that your computer/server won't go to sleep
+# Running no more than 200 samples in each batch is recommended
+# For each batch, 30 to 60 minutes might be required, please make sure that your computer/server won't go to sleep
 # Put ".panel.xlsx", ".pool.xlsx", and ".vcf.gz" files in the same folder with this script
-# Open this script directly from the parent folder with RStudio to start (just click the "Source" button)
+# Launch RStudio, go to File menu and Open File > select the script, then go to Session menu and Set Working Directory > To Source File Location
+# Run the script
 
 
 # Load R packages
